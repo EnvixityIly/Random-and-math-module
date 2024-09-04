c240ed27aa59c4abafae69cf5a22d03377c365d1
Zabir Hasan <dominicancongo@gmail.com>
2024-09-04 21:54:11 +0600

    lala

---
+++ numbergame.py
@@ -0,0 +1,20 @@
+# Danish bhai please send feet pics pookie
+import random 
+playing = True
+number = random.randint(10,20)
+
+print("I will generate a number from 10 to 20, and you have to guess the number one digit at a time.")
+
+print("The game ends when you get 1 hero!")
+
+while playing:
+
+    guess = int(input("Give me your best guess : "))
+
+    if number == guess:
+        print("You win the game")
+        print("The  number was ", number)
+        playing = False
+    
+    else: 
+        print("Your guess isnt quite right,try again and send me feet pics danish daddy!!")
